f54b2bd654894e418c0c51225367ca6c4e0a8c88
rojinosman <rojinosman1988@gmail.com>
2025-11-05 11:18:26 -0800

    changed active camera to cam0 for yolo

--- src/vision/yolo_live.py
+++ src/vision/yolo_live.py
@@ -11,9 +11,10 @@ import cv2
 model = YOLO("yolov8n.pt")
 
 # Jetson Nano CSI camera pipeline
-gst = ("nvarguscamerasrc ! "
+gst = ("nvarguscamerasrc sensor-id=0 ! "  # change 0 → 1 if your camera is on CAM1
        "video/x-raw(memory:NVMM),width=640,height=480,framerate=30/1 ! "
        "nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! appsink")
+
 
 cap = cv2.VideoCapture(gst, cv2.CAP_GSTREAMER)
 if not cap.isOpened():
